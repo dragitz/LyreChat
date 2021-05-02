a958ccb40dde94f4e132ff210f7030ef32fc9aab
dragitz <gersinmc@gmail.com>
2021-05-03 01:06:23 +0200

    Initial commit

---
+++ LyreChat/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace LyreChat
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Say = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.narrator = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Say
+            // 
+            this.Say.Location = new System.Drawing.Point(333, 57);
+            this.Say.Name = "Say";
+            this.Say.Size = new System.Drawing.Size(75, 23);
+            this.Say.TabIndex = 1;
+            this.Say.Text = "Say";
+            this.Say.UseVisualStyleBackColor = true;
+            this.Say.Click += new System.EventHandler(this.Say_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Max 500 characters";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(29, 59);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(298, 106);
+            this.Input.TabIndex = 3;
+            this.Input.Text = "";
+            // 
+            // narrator
+            // 
+            this.narrator.FormattingEnabled = true;
+            this.narrator.Items.AddRange(new object[] {
+            "ethan",
+            "don",
+            "malcom",
+            "carla",
+            "emily",
+            "nancy"});
+            this.narrator.Location = new System.Drawing.Point(334, 121);
+            this.narrator.Name = "narrator";
+            this.narrator.Size = new System.Drawing.Size(121, 21);
+            this.narrator.TabIndex = 4;
+            this.narrator.Text = "don";
+            this.narrator.SelectedIndexChanged += new System.EventHandler(this.narrator_SelectedIndexChanged);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 204);
+            this.Controls.Add(this.narrator);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Say);
+            this.Name = "Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Say;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.ComboBox narrator;
+    }
+}
+
---
+++ LyreChat/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Windows.Forms;
+
+namespace LyreChat
+{
+
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+        public static class Globals
+        {
+
+            
+            public static String PHRASE = "";
+            public static String PHRASE_OLD = "";
+
+        }
+
+        private async void Say_Click(object sender, EventArgs e)
+        {
+
+            Globals.PHRASE = Input.Text;
+
+            //This IF prevents requesting again the same phrase, it's faster
+            if (Globals.PHRASE != "")
+            {
+                if (Globals.PHRASE != Globals.PHRASE_OLD)
+                {
+                    Globals.PHRASE_OLD = Input.Text;
+
+                    var handler = new HttpClientHandler();
+
+                    // If you are using .NET Core 3.0+ you can replace `~DecompressionMethods.None` to `DecompressionMethods.All`
+                    handler.AutomaticDecompression = ~DecompressionMethods.None;
+                    using (var httpClient = new HttpClient(handler))
+                    {
+                        using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://public.lyrebird.ai/api/public/overdub"))
+                        {
+
+                            //Here we take the input
+                            request.Content = new StringContent("{\"voice_id\":\"" + narrator.Text + "\",\"text\":\"" + Input.Text + "\"}");
+                            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                            //send request to site
+                            var send = await httpClient.SendAsync(request);
+
+                            //We save it (in the .exe folder)
+                            File.WriteAllBytes("output.wav", await send.Content.ReadAsByteArrayAsync());
+
+                            SoundPlayer my_wave_file = new SoundPlayer("output.wav");
+                            my_wave_file.PlaySync();
+
+
+                        }
+                    }
+                }
+                else
+                {
+                    //We play the audio
+                    SoundPlayer my_wave_file = new SoundPlayer("output.wav");
+                    my_wave_file.PlaySync();
+                }
+            }
+        }
+
+        private void narrator_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Globals.PHRASE_OLD = "";
+        }
+    }
+}
